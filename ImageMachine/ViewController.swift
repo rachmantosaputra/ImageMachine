1d2327d6130abde7c2733131eef12b591a09f5a6
Rachmanto Saputra <rachmantosaputra@outlook.jp>
2021-08-18 21:11:18 +0700

    Initial Commit

---
+++ ImageMachine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageMachine
+//
+//  Created by Rachmanto Saputra on 18/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
